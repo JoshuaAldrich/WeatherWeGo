50ef4a7be468badb33b8676c49f2b415359c45ff
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-09-11 19:06:39 -0400

    functions created to finish most of the layout of the weather app

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -20,6 +20,8 @@ let currentHumidty = document.querySelector(".humidity");
 let currentWindSpeed = document.querySelector(".windSpeed");
 let currentUVIndex = document.querySelector(".uvIndex");
 let pastSearch = document.querySelector(".recentSearch");
+let forecast5Day = document.querySelector(".forecast");
+let citiesContainer = document.querySelector(".localStorage");
 const apiKey = "bb92cbec3b50947b23fb22f9708980b8";
 
 //Get coordinates for chosen city
@@ -43,17 +45,75 @@ let getWeather = async function (city) {
     let apiUrl5Day = `https://api.openweathermap.org/data/2.5/forecast?lat=${latLon[0].lat}&lon=${latLon[0].lon}&appid=${apiKey}`;
     let cityInformation5Day = await fetch(apiUrl5Day);
     let cityWeather5Day = await cityInformation5Day.json();
-    displayWeather(cityWeather);
-    displayWeather(cityWeather5Day);
+    let apiUrluvi = `https://api.openweathermap.org/data/2.5/uvi?lat=${latLon[0].lat}&lon=${latLon[0].lon}&appid=${apiKey}`;
+    let cityInformationuvi = await fetch(apiUrluvi);
+    let cityWeatheruvi = await cityInformationuvi.json();
+    let list = [];
+    cityWeather5Day.list.forEach(function (item) {
+      let foundItem = list.find(function (findItem) {
+        if (
+          item.dt_txt.split(" ")[0].split("-")[2] ==
+          findItem.dt_txt.split(" ")[0].split("-")[2]
+        )
+          return true;
+        else return false;
+      });
+      if (foundItem) {
+      } else {
+        list.push(item);
+      }
+    });
+    list.shift(0);
+    cityWeather.uvi = cityWeatheruvi;
+    displayWeather(cityWeather, list);
+    store(city);
+    let div = document.createElement("div");
+    div.innerHTML = city;
+    citiesContainer.appendChild(div);
   } catch (error) {
     alert("Connection to OpenWeather API not working.");
   }
 };
 
-function displayWeather(data) {
-  console.log(data);
+//convert kelvin to fahrenheit
+function kelvinToFahrenheit(temp) {
+  return (1.8 * (temp - 273) + 32).toFixed(1);
 }
 
+//Displays weather to website
+function displayWeather(currentWeather, currentWeather5Day) {
+  console.log(currentWeather, currentWeather5Day);
+  currentTemperature.innerHTML =
+    "Temperature:" + kelvinToFahrenheit(currentWeather.main.temp);
+  currentHumidty.innerHTML = "Humidity:" + currentWeather.main.humidity;
+  currentWindSpeed.innerHTML = "Wind Speed:" + currentWeather.wind.speed;
+  currentUVIndex.innerHTML = "UV index:" + currentWeather.uvi.value;
+  currentWeather5Day.forEach(function (item) {
+    let div = document.createElement("div");
+    div.innerHTML = `
+    <p>${item.dt_txt.split(" ")[0]}</p>
+    <p>Humidity: ${item.main.humidity} %</p>
+    <p>Temperature: ${kelvinToFahrenheit(item.main.temp)}</p>
+    `;
+    forecast5Day.appendChild(div);
+  });
+}
+
+//Click search button
 searchButton.addEventListener("click", function () {
   getWeather(searchBar.value);
 });
+
+//Local storage function
+function store(city) {
+  let currentStorage = JSON.parse(localStorage.getItem("cityNames"));
+  if (!currentStorage) {
+    currentStorage = [];
+  }
+  if (currentStorage.includes(city)) {
+  } else {
+    currentStorage.push(city);
+  }
+  console.log(currentStorage);
+  localStorage.setItem("cityNames", JSON.stringify(currentStorage));
+}
